b8f68b437ffb75865588d613c51aa7650a6b5bd9
Sakhawat025 <sakhawathossai25@gmail.com>
2026-07-28 21:53:53 +0600

    server and express app setup

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+
+app.use(express.json());
+
+
+app.get("/",(req,res)=>{
+    res.json({
+        message:"Task API Server Running"
+    });
+});
+
+
+module.exports = app;
